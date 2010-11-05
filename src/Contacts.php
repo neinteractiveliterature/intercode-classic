deaced54264672b72c6e7a2fde576da8d400c5f2
Nat Budin <natbudin@gmail.com>
2010-11-05 15:18:16 +0000

    Fix DB connect error

--- src/Contacts.php
+++ src/Contacts.php
@@ -1,6 +1,14 @@
 <?
 include ("intercon_db.inc");
 
+// Connect to the database
+
+if (! intercon_db_connect ())
+{
+  display_mysql_error ('Failed to establish connection to the database');
+  exit ();
+}
+
 // Display boilerplate
 
 html_begin ();
