6454f178f1920b0b67663247f06bc5de3c7e48ce
Nat Budin <natbudin@gmail.com>
2011-09-11 19:24:41 +0000

    Branch for L, revert Westin rooms

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "intercon-l"
 set :user, "neiladmin"
 set :domain, "#{user}@apocalypse.dreamhost.com"
-set :repository, "file:///home/#{user}/svn/intercon/trunk/src"
+set :repository, "file:///home/#{user}/svn/intercon/branches/L/src"
 
 task :sandbox do
   set :deploy_to, "/home/#{user}/sandbox.interactiveliterature.org"
