e3758ae59a913e7b4e4c1ce677cd02a882beea59
Nat Budin <natbudin@gmail.com>
2014-11-09 19:38:06 +0000

    Make old branches deployable again

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 set :application, "intercon-n"
 set :user, "neiladmin"
-set :domain, "#{user}@apocalypse.dreamhost.com"
+set :domain, "#{user}@transit.dreamhost.com"
 set :repository, "file:///home/#{user}/svn/intercon/branches/N/src"
 set :skip_scm, false
 
