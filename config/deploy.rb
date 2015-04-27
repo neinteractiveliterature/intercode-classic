ae725503de4987553e11366fe3a67d922cab741f
Nat Budin <nbudin@patientslikeme.com>
2015-04-27 10:30:06 -0400

    Update deploy scripts for git + vps1
    Conflicts:
    	Gemfile
    	Gemfile.lock
    	config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,17 @@
 set :application, "intercon-j"
-set :user, "neiladmin"
-set :domain, "#{user}@apocalypse.dreamhost.com"
-set :repository, "file:///home/#{user}/svn/intercon/branches/j/src"
-set :deploy_to, "/home/#{user}/#{application}"
+set :user, "deploy"
+set :domain, "#{user}@vps1.interconlarp.org"
+set :repository, "git@vps1.interconlarp.org:intercode1.git"
+set :revision, "origin/j"
+set :skip_scm, false
+
+task :sandbox do
+  set :deploy_to, "/var/www/sandbox.interactiveliterature.org"
+end
+
+task :production do
+  set :deploy_to, "/var/www/#{application}"
+end
 
 namespace :vlad do
   Rake.clear_tasks('vlad:update_symlinks')
@@ -15,8 +24,8 @@ do
   if [[ $FILENAME != *~ ]]
   then
     echo "Linking in local copy of $f"
-    rm -f #{release_path}/$FILENAME
-    ln -s #{shared_path}/local/$FILENAME #{release_path}/$FILENAME
+    rm -f #{release_path}/src/$FILENAME
+    ln -s #{shared_path}/local/$FILENAME #{release_path}/src/$FILENAME
   fi
 done
 EOF
